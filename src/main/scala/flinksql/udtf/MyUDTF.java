033c376d928c4bef865f38466c585ee7db32896b
lj <liujian9009@126.com>
2020-03-03 11:43:37 +0800

    flink-yarn

---
+++ src/main/scala/flinksql/udtf/MyUDTF.java
@@ -0,0 +1,30 @@
+package flinksql.udtf;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.table.api.Types;
+import org.apache.flink.table.functions.TableFunction;
+import org.apache.flink.types.Row;
+
+public class MyUDTF extends TableFunction<Row> {
+
+    public void eval(String s){
+        JSONArray jsonArray = JSONArray.parseArray(s);
+        for(int i =0; i < jsonArray.size(); i++){
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String type = jsonObject.getString("type");
+            String value = jsonObject.getString("value");
+            collector.collect(Row.of(type, value));
+
+        }
+
+    }
+
+
+    @Override
+    public TypeInformation<Row> getResultType(){
+        return Types.ROW(Types.STRING(),Types.STRING());
+    }
+
+}
