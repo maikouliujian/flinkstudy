a2296c4fb265f11c1ebb3727a15d71c829b666bd
刘剑 <liujian@17shanyuan.com>
2020-05-18 10:58:58 +0800

    table api

---
+++ src/main/java/com/mine/dw/EventTimeBucketAssigner.java
@@ -0,0 +1,42 @@
+package com.mine.dw;
+
+import org.apache.flink.core.io.SimpleVersionedSerializer;
+import org.apache.flink.streaming.api.functions.sink.filesystem.BucketAssigner;
+import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.SimpleVersionedStringSerializer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author lj
+ * @createDate 2020/5/12 9:12
+ **/
+public class EventTimeBucketAssigner implements BucketAssigner<String, String> {
+
+
+    @Override
+    public String getBucketId(String element, Context context) {
+        String partitionValue;
+        try {
+            partitionValue = getPartitionValue(element);
+        } catch (Exception e) {
+            partitionValue = "00000000";
+        }
+        return "dt=" + partitionValue;//分区目录名称
+    }
+
+    @Override
+    public SimpleVersionedSerializer<String> getSerializer() {
+
+        return SimpleVersionedStringSerializer.INSTANCE;
+    }
+
+    private String getPartitionValue(String element) throws Exception {
+
+        // 取出最后拼接字符串的es字段值，该值为业务时间
+        long eventTime = Long.parseLong(element.split(",")[1]);
+        Date eventDate = new Date(eventTime);
+        return new SimpleDateFormat("yyyyMMdd").format(eventDate);
+
+    }
+}
